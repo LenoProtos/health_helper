37fc43184ab16a9f9a7b8f1f1bd697f8e814854c
LenoProtos <72996315+LenoProtos@users.noreply.github.com>
2021-02-16 19:53:44 +0300

    Update sr.py

--- sr.py
+++ sr.py
@@ -1,26 +1,43 @@
-from pydub import AudioSegment
-import speech_recognition as sr
-import os
+import telebot
 import requests
+import speech_recognition as sr
+import os.path
+import pandas
+import subprocess
+import soundfile as sf
 
-@bot.message_handler(content_types=['voice','text'])       #https://qna.habr.com/q/768647
+token = ''
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(content_types=['voice','text'])
 def repeat_all_message(message):
   file_info = bot.get_file(message.voice.file_id)
   file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(token, file_info.file_path))
 
-#sound = AudioSegment.from_mp3("/voice/file.mp3")          
-#sound.export("/output/path/file.wav", format="wav")
+  with open('voice.ogg','wb') as f:
+    f.write(file.content)
 
-r = sr.Recognizer()
-h = sr.AudioFile('/voice/file.wav')
-with h as s:
-  a = r.record(s)
-  tupe(a)
-  t = r.recognize_google(audio, language= 'ru-RU')
-  print({t.lower()})
-  
-  
-path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'file.mp3')
-os.remove(path)
-path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'file.wav')
-os.remove(path)
+if __name__ == '__main__':
+   bot.polling(none_stop=True)
+
+if os.path.exists("voice.ogg")==1:
+    print(os.path.exists("voice.ogg"))
+    cd = r"C:\Users\admin\PycharmProjects\untitled2"  #ffmpeg-n4.3.1-221-gd08bcbffff-win64-gpl-4.3
+    command = "ffmpeg -i voice.ogg voice3.wav"        #https://www.youtube.com/watch?v=qjtmgCb8NcE&t=335s
+    #subprocess.run(cd)
+    #subprocess.run(command)
+    os.system(cd)
+    os.system(command)
+
+
+
+
+if os.path.exists("2voice.wav")==1:
+    r = sr.Recognizer()
+    harvard = sr.AudioFile('2voice.wav')
+    with harvard as source:
+        audio = r.record(source)
+        type(audio)
+        a = r.recognize_google(audio, language= 'ru-RU')
+    tex = {a.lower()}
+    print(tex)
